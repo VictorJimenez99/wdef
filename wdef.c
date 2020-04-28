e60c5fd4dca14e3e2a2274f2d93a3ec9fd90c27e
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-04-28 15:43:05 -0500

    improved find_file function

--- wdef.c
+++ wdef.c
@@ -2,14 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define SEARCH_DIR "/usr/local/wdef/"
+//!CHANGE THIS DEFINE TO WHEREVER YOU HAVE THE WDEF FOLDER
+//TODO READ FROM A FILE
+#define SEARCH_DIR "/home/victor/wdef/"
 
 void print_file(FILE *definition);
 FILE *find_file(char *folder, char *name);
 
 int main(int argc, char *argv[])
 {
-    printf("word definition\n");
+    printf("word definition %03d\n", argc);
     FILE *definition = find_file("es", "a");
     print_file(definition);
     fclose(definition);
@@ -28,14 +30,22 @@ void print_file(FILE *definition)
     }
 }
 
+//*This function concatenates the SEARCH_DIR with folder and name and returns a file descriptor
 FILE *find_file(char *folder, char *name)
 {
     FILE *location;
-    char base[100];
-    strcpy(base, SEARCH_DIR);
-    strcat(base, folder);
-    strcat(base, "/");
-    strcat(base, name);
-    location = fopen(base, "r");
+    char path[100];
+    char first_letter[2];
+    first_letter[0] = name[0];
+    first_letter[1] = '\0';
+    strcpy(path, SEARCH_DIR);
+    strcat(path, folder);
+    strcat(path, "/");
+    strcat(path, first_letter);
+    strcat(path, "/");
+    strcat(path, name);
+    //printf("%s", path);
+    //fflush(stdout);
+    location = fopen(path, "r");
     return location;
 }
