b5f4f109eff037a9bf2a2f6934666aa18955c26a
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-05-03 17:10:51 -0500

    added create_flag method

--- wdef.c
+++ wdef.c
@@ -4,47 +4,64 @@
 #include "file_man.h"
 #include "mem_man.h"
 
+static inline int create_flag(int folder_counter, int word_counter, int section_counter);
+
 int main(int argc, char *argv[])
 {
-    printf("argc: %d\n", argc);
-    fflush(stdout);
     int folder_counter = 0;
     int word_counter = 0;
     int section_counter = 0;
 
     count_arguments(&folder_counter, &word_counter, &section_counter, argc, argv);
+    char **words = create2d_array(word_counter, 30);
+    char **folders = create2d_array(folder_counter, 30);
+    char **sections = create2d_array(section_counter, 30);
 
-    printf("word_counter: %d\nfolder_counter: %d\nsection_counter: %d\n", word_counter, folder_counter, section_counter);
-    fflush(stdout);
-    char **words = (char **)malloc(sizeof(char *) * word_counter);
-    if (words == NULL)
-    {
-        perror("MEMORY ERROR");
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i != word_counter; i++)
-    {
-        words[i] = (char *)malloc(sizeof(char) * 30);
-        if (words[i] == NULL)
-        {
-            perror("MEMORY ERROR");
-            exit(EXIT_FAILURE);
-        }
-    }
+    char option = create_flag(folder_counter, word_counter, section_counter);
 
-    strcpy(words[0], "hola");
-    printf("%s", words[0]);
+    printf("what needs to be done: %d\n", option);
+    /**
+     * 0-NoArguments    1-FolderArgument    2-WordArgument  3-Folder&WordArguments
+     * 4-SecctionArg    5-Seccion&Folder    6-Section&Word  7-Section&Word&Folder
+     * 
+     * */
 
     fflush(stdout);
-
-    printf("%d", folder_counter);
-    printf("word definition");
-    FILE *definition = find_file("mineriaDeDatos", "tratamientoDeDatos");
-    print_file(definition);
-    fclose(definition);
+    //FILE *definition; // = find_file("mineriaDeDatos", "tratamientoDeDatos");
+    //print_file(definition);
+    //fclose(definition);
+    fflush(stdout);
+    //word_counter++;
     if (free2d_array(words, word_counter) != 0)
     {
         perror("FREE ERROR");
         exit(EXIT_FAILURE);
     }
+    if (free2d_array(folders, folder_counter) != 0)
+    {
+        perror("FREE ERROR");
+        exit(EXIT_FAILURE);
+    }
+    if (free2d_array(sections, section_counter) != 0)
+    {
+        perror("FREE ERROR");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static inline int create_flag(int folder_counter, int word_counter, int section_counter)
+{
+    int fflag = 0, wflag = 0, sflag = 0;
+    if (folder_counter > 0)
+        fflag = 1;
+    if (word_counter > 0)
+        wflag = 2;
+    if (section_counter > 0)
+        sflag = 4;
+    return fflag | wflag | sflag;
+    /**
+     * 0-NoArguments    1-FolderArgument    2-WordArgument  3-Folder&WordArguments
+     * 4-SecctionArg    5-Seccion&Folder    6-Section&Word  7-Section&Word&Folder
+     * 
+     * */
 }
