70871ff4980552a08a40fd5cfde0a1a05c6508b6
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-05-03 17:11:30 -0500

    created "create2d_array" fuction

--- mem_man.c
+++ mem_man.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//returns 0 if everything went ok, returns -1 if the array is already free
+//returns -2 if a pointer of the second dimension is free
 int free2d_array(char **array, int rows)
 {
+    if (rows == 0)
+        return 0;
     if (array == NULL)
     {
         return -1;
@@ -20,6 +24,26 @@ int free2d_array(char **array, int rows)
     }
     free(array);
     return 0;
+}
+
+char **create2d_array(int n_pointers, int sizeof_str)
+{
+    char **array = (char **)malloc(sizeof(char *) * n_pointers);
+    if (array == NULL)
+    {
+        perror("MEMORY ERROR");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i != n_pointers; i++)
+    {
+        array[i] = (char *)malloc(sizeof(char) * sizeof_str);
+        if (array[i] == NULL)
+        {
+            perror("MEMORY ERROR");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return array;
 }
 
 void count_arguments(int *folder_counter, int *word_counter, int *section_counter, int argc, char **argv)
--- mem_man.h
+++ mem_man.h
@@ -3,4 +3,5 @@
 
 int free2d_array(char **array, int rows);
 void count_arguments(int *folder_counter, int *word_counter, int *section_counter, int argc, char **argv);
+char **create2d_array(int n_pointers, int sizeof_str);
 #endif
