587f848518f571d8786928a3c58807cd35b89d69
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-05-03 22:29:37 -0500

    classify arguments

--- wdef.c
+++ wdef.c
@@ -4,7 +4,9 @@
 #include "file_man.h"
 #include "mem_man.h"
 
-static inline int create_flag(int folder_counter, int word_counter, int section_counter);
+int create_flag(int folder_counter, int word_counter, int section_counter);
+void gather_args(char **array, int counter, char **argv, int option);
+void print2d_array(char **array, int counter);
 
 int main(int argc, char *argv[])
 {
@@ -19,14 +21,46 @@ int main(int argc, char *argv[])
 
     char option = create_flag(folder_counter, word_counter, section_counter);
 
+    gather_args(folders, folder_counter, argv, 0);
+    gather_args(words, word_counter, argv, 1);
+    gather_args(sections, section_counter, argv, 2);
+    print2d_array(folders, folder_counter);
+    print2d_array(words, word_counter);
+    print2d_array(sections, section_counter);
+
     printf("what needs to be done: %d\n", option);
     /**
      * 0-NoArguments    1-FolderArgument    2-WordArgument  3-Folder&WordArguments
      * 4-SecctionArg    5-Seccion&Folder    6-Section&Word  7-Section&Word&Folder
      * 
      * */
-
-    fflush(stdout);
+    switch (option)
+    {
+    case 2:
+        /**
+         * for each word
+         *  seach in every folder
+         *   print matches
+        */
+        break;
+    case 3:
+        /**
+         * for each folder
+         *  for each file
+         *   printfile
+        */
+        break;
+    case 7:
+        /**
+         * for each folder
+         *  for each file
+         *   print section
+        */
+        break;
+    default:
+        printf("NOT ENOUGH ARGUMENTS\ntry $wdef @[folder_name] -[section(optional)] [word]\n");
+        break;
+    }
     //FILE *definition; // = find_file("mineriaDeDatos", "tratamientoDeDatos");
     //print_file(definition);
     //fclose(definition);
@@ -49,7 +83,7 @@ int main(int argc, char *argv[])
     }
 }
 
-static inline int create_flag(int folder_counter, int word_counter, int section_counter)
+int create_flag(int folder_counter, int word_counter, int section_counter)
 {
     int fflag = 0, wflag = 0, sflag = 0;
     if (folder_counter > 0)
@@ -64,4 +98,69 @@ static inline int create_flag(int folder_counter, int word_counter, int section_
      * 4-SecctionArg    5-Seccion&Folder    6-Section&Word  7-Section&Word&Folder
      * 
      * */
+}
+void gather_args(char **array, int counter, char **argv, int option)
+{
+    argv = argv + 1;
+    if (option < 0 || option > 2) //its not beeing used properly
+    {
+        perror("INTERNAL ERROR: GATHER_ARGS FUNCTION");
+        exit(EXIT_FAILURE);
+    }
+    if (counter == 0)
+        return;
+    int aux = 0;
+    char *str;
+
+    if (option == 1)
+    {
+        //word
+        printf("gathering words\n");
+        fflush(stdout);
+        while (counter != 0)
+        {
+            if (argv[aux][0] != '@' && argv[aux][0] != '-')
+            {
+                str = argv[aux];
+                strcpy(array[counter - 1], str);
+                counter--;
+            }
+            aux++;
+        }
+        return;
+    }
+    else
+    {
+        //folder or section
+        printf("gathering data\n");
+        fflush(stdout);
+        char ch;
+        if (option == 0)
+            ch = '@'; //folder
+        else if (option == 2)
+            ch = '-'; //section
+
+        while (counter != 0)
+        {
+            if (argv[aux][0] == ch)
+            {
+                str = argv[aux];
+                str++;
+                strcpy(array[counter - 1], str);
+                counter--;
+            }
+
+            aux++;
+        }
+        return;
+    }
+}
+
+void print2d_array(char **array, int counter)
+{
+    printf("\nDEBUG, printing 2d array:\n");
+    for (int i = 0; i != counter; i++)
+    {
+        printf("%s\n", array[i]);
+    }
 }
